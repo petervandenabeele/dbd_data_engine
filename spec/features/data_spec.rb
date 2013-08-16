e682a0c56718590e823e7a53e8508621af811825
Peter Vandenabeele <peter@vandenabeele.com>
2013-08-16 15:24:23 +0200

    Refactor the spec (and decide to move it)
    * add /data/contexts for contexts
    * add /data/resources for resources

--- spec/features/data_spec.rb
+++ spec/features/data_spec.rb
@@ -1,44 +1,44 @@
 require 'spec_helper'
 
 module DbdDataEngine
-  describe "Data" do
-    describe "GET /data" do
-      context "routing" do
-        it "when the engine is mounted under /" do
+  describe 'Data' do
+    describe 'GET /data' do
+      context 'routing' do
+        it 'when the engine is mounted under /' do
           dbd_data_engine.data_index_path.should == '/data'
         end
       end
 
-      context "page content" do
+      context 'page content' do
 
         before(:each) do
           visit(dbd_data_engine.data_index_path)
         end
 
-        it "shows the data" do
+        it 'shows the data' do
           expect(page).to have_text('Data')
         end
       end
     end
 
-    describe "GET /data/new" do
-      context "routing" do
-        it "when the engine is mounted under /" do
+    describe 'GET /data/new' do
+      context 'routing' do
+        it 'when the engine is mounted under /' do
           dbd_data_engine.new_data_path.should == '/data/new'
         end
       end
 
-      context "page content" do
+      context 'page content' do
 
         before(:each) do
           visit(dbd_data_engine.new_data_path)
         end
 
-        it "talks about a new resource" do
+        it 'talks about a new resource' do
           expect(page.text).to match(/new resource/i)
         end
 
-        it "has a select box with schema:givenName as option" do
+        it 'has a select box with schema:givenName as option' do
           expect(page).to have_select('predicate', options: ['schema:givenName','schema:familyName'])
         end
       end
