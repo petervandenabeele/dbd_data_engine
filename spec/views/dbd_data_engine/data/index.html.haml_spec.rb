9e74611558c4f94e414f336d5bf0492a25a450f1
Peter Vandenabeele <peter@vandenabeele.com>
2013-09-03 22:34:59 +0200

    ../data/index had links to contexts and resources

--- spec/views/dbd_data_engine/data/index.html.haml_spec.rb
+++ spec/views/dbd_data_engine/data/index.html.haml_spec.rb
@@ -4,4 +4,15 @@ describe 'dbd_data_engine/data/index.html.haml' do
   it 'renders' do
     render
   end
+
+  it 'links to resources' do
+    render
+    rendered.should match(%r{<a href="/data/resources">resources</a>})
+  end
+
+  it 'links to contexts' do
+    render
+    rendered.should match(%r{<a href="/data/contexts">contexts</a>})
+  end
+
 end
