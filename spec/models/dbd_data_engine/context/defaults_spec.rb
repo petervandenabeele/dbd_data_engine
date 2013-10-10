b90c4378555d31d3da8c9231841d8092e2d20b8b
Peter Vandenabeele <peter@vandenabeele.com>
2013-10-10 23:16:08 +0200

    Move a block of tests 1 level indent to the right
    * this yields an ugly diff message, while it really just adds
      1 level of indent ...

--- spec/models/dbd_data_engine/context/defaults_spec.rb
+++ spec/models/dbd_data_engine/context/defaults_spec.rb
@@ -15,75 +15,78 @@ module DbdDataEngine
       context.select{ |cf| cf.object.to_s == object }.size.should == 1
     end
 
-    context 'default_from_params' do
-      it 'returns public_today' do
-        context = described_class.default_from_params('public today')
-        assert_object(context, 'public')
+    context 'default context does not yet exist' do
+
+      context 'default_from_params' do
+        it 'returns public_today' do
+          context = described_class.default_from_params('public today')
+          assert_object(context, 'public')
+        end
+
+        it 'returns personal_today' do
+          context = described_class.default_from_params('personal today')
+          assert_object(context, 'personal')
+        end
+
+        it 'returns business_today' do
+          context = described_class.default_from_params('business today')
+          assert_object(context, 'business')
+        end
+
+        it 'raises for inexisting param' do
+          lambda{ described_class.default_from_params('foo') }.should raise_error(/foo/)
+        end
       end
 
-      it 'returns personal_today' do
-        context = described_class.default_from_params('personal today')
-        assert_object(context, 'personal')
+      context 'public_today' do
+
+        let(:context) { described_class.public_today}
+
+        it 'exists' do
+          context
+        end
+
+        it 'has 6 context_facts' do
+          context.select{ |cf| cf.class == Dbd::ContextFact }.size.should == 6
+        end
+
+        it 'has a public object' do
+          assert_object(context, 'public')
+        end
       end
 
-      it 'returns business_today' do
-        context = described_class.default_from_params('business today')
-        assert_object(context, 'business')
+      context 'personal_today' do
+
+        let(:context) { described_class.personal_today}
+
+        it "exists" do
+          context
+        end
+
+        it "has 6 context_facts" do
+          context.select{ |cf| cf.class == Dbd::ContextFact}.size.should == 6
+        end
+
+        it 'has a personal object' do
+          assert_object(context, 'personal')
+        end
       end
 
-      it 'raises for inexisting param' do
-        lambda{ described_class.default_from_params('foo') }.should raise_error(/foo/)
-      end
-    end
+      context 'business_today' do
 
-    context 'public_today' do
+        let(:context) { described_class.business_today}
 
-      let(:context) { described_class.public_today}
+        it "exists" do
+          context
+        end
 
-      it 'exists' do
-        context
-      end
+        it "has 6 context_facts" do
+          context.select{ |cf| cf.class == Dbd::ContextFact}.size.should == 6
+        end
 
-      it 'has 6 context_facts' do
-        context.select{ |cf| cf.class == Dbd::ContextFact }.size.should == 6
-      end
-
-      it 'has a public object' do
-        assert_object(context, 'public')
-      end
-    end
-
-    context 'personal_today' do
-
-      let(:context) { described_class.personal_today}
-
-      it "exists" do
-        context
-      end
-
-      it "has 6 context_facts" do
-        context.select{ |cf| cf.class == Dbd::ContextFact}.size.should == 6
-      end
-
-      it 'has a personal object' do
-        assert_object(context, 'personal')
-      end
-    end
-
-    context 'business_today' do
-
-      let(:context) { described_class.business_today}
-
-      it "exists" do
-        context
-      end
-
-      it "has 6 context_facts" do
-        context.select{ |cf| cf.class == Dbd::ContextFact}.size.should == 6
-      end
-
-      it 'has a business object' do
-        assert_object(context, 'business')
+        it 'has a business object' do
+          assert_object(context, 'business')
+        end
       end
     end
   end
