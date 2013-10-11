d92bc76a82436dd0566302f3f4aeb2de45418372
Peter Vandenabeele <peter@vandenabeele.com>
2013-10-11 23:39:35 +0200

    Add navigation to the resource created view

--- spec/views/dbd_data_engine/resources/create.html.haml_spec.rb
+++ spec/views/dbd_data_engine/resources/create.html.haml_spec.rb
@@ -9,4 +9,22 @@ describe 'dbd_data_engine/resources/create.html.haml' do
     @context = [context_fact]
     render
   end
+
+  context 'navigation' do
+    it 'points to "resources"' do
+      render.should have_css("a[href='/data/resources']", text: 'resources')
+    end
+
+    it 'points to "contexts"' do
+      render.should have_css("a[href='/data/contexts']", text: 'contexts')
+    end
+
+    it 'points to "new resource"' do
+      render.should have_css("a[href='/data/resources/new']", text: 'new resource')
+    end
+
+    it 'points to "new context"' do
+      render.should have_css("a[href='/data/contexts/new']", text: 'new context')
+    end
+  end
 end
