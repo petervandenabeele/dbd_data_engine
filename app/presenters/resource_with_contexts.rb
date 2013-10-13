eaaa545990fa1791fa817dc6bfe7b4770c1765eb
Peter Vandenabeele <peter@vandenabeele.com>
2013-10-13 16:53:16 +0200

    Implement ResourceWithContexts

--- app/presenters/resource_with_contexts.rb
+++ app/presenters/resource_with_contexts.rb
@@ -1,2 +1,16 @@
 class ResourceWithContexts
+  include Enumerable
+
+  def initialize(options)
+    @resource = options[:resource]
+    @graph = options[:graph]
+    raise 'resource: is a required option' unless @resource
+    raise 'graph: is a required option' unless @graph
+  end
+
+  def each
+    @resource.each do |fact|
+      yield FactWithContext.new(fact: fact, graph: @graph)
+    end
+  end
 end
--- spec/presenters/resource_with_contexts/new_spec.rb
+++ spec/presenters/resource_with_contexts/new_spec.rb
@@ -1,7 +1,32 @@
 require 'spec_helper'
 
 describe ResourceWithContexts do
-  it 'klass.new works' do
-    subject
+  context 'klass.new with a resource and a graph' do
+
+    let(:graph) { TestFactories::Graph.full }
+    let(:resource) { [graph.last] }
+    let(:resource_with_contexts) do
+      described_class.new(
+        resource: resource,
+        graph: graph)
+    end
+
+    it 'is enumerable' do
+      resource_with_contexts.map #should_not raise_error
+    end
+
+    it 'has FactWithContext as members' do
+      resource_with_contexts.first.should be_a(FactWithContext)
+    end
+
+    context 'raises when new is not given all arguments' do
+      it 'without resource' do
+        lambda{ described_class.new(graph: graph) }.should raise_error
+      end
+
+      it 'without graph' do
+        lambda{ described_class.new(resource: resource) }.should raise_error
+      end
+    end
   end
 end
