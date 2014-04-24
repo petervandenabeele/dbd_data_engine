bc3255da9eaa10307a6680e2922d8f1cb5284220
Peter Vandenabeele <peter@vandenabeele.com>
2014-04-24 03:58:51 +0200

    Test the usage of predicates from dbd

--- app/models/dbd_data_engine/resource.rb
+++ app/models/dbd_data_engine/resource.rb
@@ -3,7 +3,6 @@ module DbdDataEngine
 
     extend ::DbdDataEngine::ResourceSelectors
 
-    # TODO test the dbd_resources
     # TODO fix the hack below (add meta to used)
     def self.used_predicates
       meta_resources = ::DbdOnto::Meta.new.resources
--- spec/models/dbd_data_engine/resource/used_predicates_spec.rb
+++ spec/models/dbd_data_engine/resource/used_predicates_spec.rb
@@ -6,6 +6,12 @@ module DbdDataEngine
 
       before(:each) do
         stub_real_data_dir
+        File.open(DbdDataEngine.default_CSV_location, "w") do |f|
+          f.write <<EOS
+"2014-04-23 06:14:40.852155565 UTC","00a77b19-c11b-47f2-8bdd-4e8680687832","a85012ee-b31d-4402-84bb-9db33cbadfdc","50f2d11d-8a0c-4832-bca9-9e240afc83bb","meta:defines_predicate","s","dbd:test"
+"2014-04-23 06:14:40.852176070 UTC","39b6bffa-008d-43b4-ae02-8cc4d4a9c3e2","a85012ee-b31d-4402-84bb-9db33cbadfdc","50f2d11d-8a0c-4832-bca9-9e240afc83bb","meta:predicate_used","s","true"
+EOS
+        end
       end
 
       let(:example_used_predicates) do
@@ -17,7 +23,7 @@ module DbdDataEngine
           ['address (schema)', 'schema:address'],
           ['familyName (schema)', 'schema:familyName'],
           ['givenName (schema)', 'schema:givenName'],
-          #['test (dbd)', 'dbd:test'],
+          ['test (dbd)', 'dbd:test'],
         ]
       end
 
